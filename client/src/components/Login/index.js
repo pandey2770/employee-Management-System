6efe957b1d5ded4ae75082f5e0276077500bf7dd
Abhishek Pandey <pandey.abhishek2770@gmail.com>
2017-10-27 12:49:39 +0530

    design

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { withRouter } from 'react-router'
+import { withRouter } from 'react-router';
 import { login } from '../../actions';
 import './styles.css';
 
 
 class Login extends Component {
+  
   state = {
     username: '',
     password: ''
@@ -15,20 +16,21 @@ class Login extends Component {
       [`${event.target.name}`]: event.target.value
     });
   };
-
+ 
   login = () => {
     const { history } = this.props;
     const { username, password } = this.state;
     login(username, password, history);
   }
-
   render() {
     const { username, password } = this.state;
-    return (
-      <div>
-        <div><input placeholder="Username" name="username" value={username} onChange={this.updateValue} /></div>
-        <div><input type="password" placeholder="Password" name="password" value={password} onChange={this.updateValue} /></div>
-        <div><input type="button" value="Login" onClick={this.login}/></div>
+    return (      
+      <div className='login'>
+        <div className='login-first'>
+          <div><input  className='input' placeholder="Username" name="username" value={username} onChange={this.updateValue} /></div>
+          <div><input  className='input' type="password" placeholder="Password" name="password" value={password} onChange={this.updateValue} /></div>
+          <div><input  className='' type="button" value="Login" onClick={this.login}/></div>
+        </div>
       </div>
     );
   }
